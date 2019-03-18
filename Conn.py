3435de95f8f08c197821f2555619cc7f3762d638
Juan Pulido <jjpulidos@unal.edu.co>
2019-03-17 19:15:04 -0500

    Fixed Bugs

--- Conn.py
+++ Conn.py
@@ -11,7 +11,7 @@ def connection_Mongo(param={}):
     connection_Mongo se encarga de conectar con la base de datos MongoDB y retorna una lista de estructuras JSON
     :return  Lista de Estructuras JSON correspondiente a las busquedas en la base de datos MongoDB:
     """
-    client_mongo = MongoClient("mongodb+srv://RUNMapp-developer:POpeMXyP1DY6kaJR@production-cluster-say4a.mongodb.net/test?retryWrites=true")
+    client_mongo = MongoClient("mongodb+srv://RUNMapp-developer:<password>@production-cluster-say4a.mongodb.net/test?retryWrites=true")
     db_mongo = client_mongo['RUNMapp_DB']
 
     return db_mongo
